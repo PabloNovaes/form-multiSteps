93e561c0828263a79386aa8c12a3f72523e841bc
PabloNovaes <ppablo.0028922@gmail.com>
2023-10-30 19:18:35 -0200

    feat: add features to form submit

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,11 @@
 export async function searchAdress(cep) {
-  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-return   response.json()
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  return response.json();
+}
+
+export async function submitForm(data) {
+  await fetch("", {
+    method: "POST",
+    body: data,
+  });
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,43 @@
-import { success, error } from './sweet.js'
-import { searchAdress } from './api.js'
+import { success, error } from "./sweet.js";
+import { searchAdress, submitForm } from "./api.js";
 
 let currentStep = 0;
 const form = document.querySelector("form");
 const formSteps = form.querySelectorAll(".form-step");
 const buttons = document.querySelectorAll("form button");
-const cepInput = document.querySelector("#cep")
+const cepInput = document.querySelector("#cep");
 
 const formMethods = {
-  checkValues() {
+  checkAllFormValues() {
     const inputs = Array.from(form.querySelectorAll(".form-step input"));
-    return inputs.every(input => {
-      return input.value !== ""
+    return inputs.every((input) => {
+      return input.value !== "";
     });
   },
+
   checkStepValues() {
     const inputs = Array.from(form.querySelectorAll(".form-step.active input"));
-    return inputs.every(input => {
-      return input.value !== ""
+    return inputs.every((input) => {
+      return input.value !== "";
     });
   },
+
   updateActiveStep() {
     formSteps.forEach((step) => step.classList.remove("active"));
     return formSteps[currentStep].classList.toggle("active");
   },
-  async autoCompleteAdress(cep) {
-    const data = await searchAdress(cep)
-return data
-  },
-  sendForm() {
-    if (!formMethods.checkValues()) {
-      return error("Preencha todos os campos")
 
+  async autoCompleteAdress(cep) {
+    const data = await searchAdress(cep);
+    return data;
+  },
+
+  async sendForm() {
+    if (!formMethods.checkAllFormValues()) {
+      return error("Preencha todos os campos");
     }
-    return success("enviado");
+    const formData = new FormData(form);
+    return formData;
   },
 };
 
@@ -42,7 +46,7 @@ buttons.forEach((btn) => {
     const actions = {
       next() {
         if (!formMethods.checkStepValues()) {
-          return error("Preencha todos os campos")
+          return error("Preencha todos os campos");
         }
         currentStep++;
         formMethods.updateActiveStep();
@@ -75,12 +79,13 @@ formSteps.forEach((step) => {
   });
 });
 
-cepInput.addEventListener("keyup", async()=>{
-  const value = cepInput.value
-  if(value.length < 8) return
-const data = await formMethods.autoCompleteAdress(value)
-   const {logradouro, bairro, localidade} = data
-   document.querySelector("input[name=rua]").value = logradouro
-   document.querySelector("input[name=bairro]").value = bairro 
-   // document.querySelector("input[name=rua]").value = 
-})
+cepInput.addEventListener("keyup", () => {
+  const value = cepInput.value;
+  if (value.length < 8) return;
+  formMethods.autoCompleteAdress(value).then((data) => {
+    const { logradouro, bairro, localidade } = data;
+    document.querySelector("input[name=rua]").value = logradouro;
+    document.querySelector("input[name=bairro]").value = bairro;
+    document.querySelector("input[name=localidade]").value = localidade;
+  });
+});
